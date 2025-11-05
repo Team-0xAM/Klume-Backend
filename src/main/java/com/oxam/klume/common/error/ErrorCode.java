3ded201fa91b431e5f020c62aa5ca6c12c2cb0c5
jiruen <kjr2699@naver.com>
2025-11-06 00:44:45 +0900

    chore: #1 - 예외 코드 및 전역 예외 처리 초기 세팅

---
+++ src/main/java/com/oxam/klume/common/error/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.oxam.klume.common.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    /* COMMON */
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON001", "Entity not found"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON002", "Internal server error");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(final HttpStatus status, final String code, final String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
--- src/main/java/com/oxam/klume/config/SwaggerConfig.java
+++ src/main/java/com/oxam/klume/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.oxam.klume.common.config;
+package com.oxam.klume.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
