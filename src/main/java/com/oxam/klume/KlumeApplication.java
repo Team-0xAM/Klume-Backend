26eadb91794447203edbb335f733706efd2e9b1c
jiruen <kjr2699@naver.com>
2025-11-06 00:11:23 +0900

    chore: #1 - 패키지 구조 초기 세팅

---
+++ src/main/java/com/oxam/klume/KlumeApplication.java
@@ -0,0 +1,11 @@
+package com.oxam.klume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlumeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KlumeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oxam/klume/KlumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxam.klume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
