62ccd3a3451bb910c3d158d49d8340aa2e5b837c
HoodRyan <bty0823@naver.com>
2025-11-08 19:02:38 +0900

    fix: #2 - pr 확인 후 var -> Authentication auth 로 변경

--- src/main/java/com/oxam/klume/auth/controller/AuthController.java
+++ src/main/java/com/oxam/klume/auth/controller/AuthController.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +46,7 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
         // Spring Security의 로그아웃 처리
-        var auth = SecurityContextHolder.getContext().getAuthentication();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
