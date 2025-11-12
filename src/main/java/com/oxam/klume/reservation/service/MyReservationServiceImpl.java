9d9e4c9bb3baf0e4f5339f58b325055e3701e8a4
Yujin <wjd2000db@gmail.com>
2025-11-12 14:54:00 +0900

    feat: #28 - 이용 가능 시작 시간이 지난 예약은 취소 불가 기능 구현

--- src/main/java/com/oxam/klume/reservation/service/MyReservationServiceImpl.java
+++ src/main/java/com/oxam/klume/reservation/service/MyReservationServiceImpl.java
@@ -3,11 +3,11 @@ package com.oxam.klume.reservation.service;
 import com.oxam.klume.organization.entity.OrganizationMember;
 import com.oxam.klume.organization.exception.OrganizationNotFoundException;
 import com.oxam.klume.organization.repository.OrganizationMemberRepository;
-import com.oxam.klume.organization.repository.OrganizationRepository;
 import com.oxam.klume.reservation.dao.MyReservationMapper;
 import com.oxam.klume.reservation.dto.MyReservationDTO;
 import com.oxam.klume.reservation.entity.DailyReservation;
 import com.oxam.klume.reservation.entity.Reservation;
+import com.oxam.klume.reservation.exception.ReservationAlreadyStartedException;
 import com.oxam.klume.reservation.exception.ReservationNotFoundException;
 import com.oxam.klume.reservation.repository.DailyReservationRepository;
 import com.oxam.klume.reservation.repository.ReservationRepository;
@@ -17,8 +17,6 @@ import com.oxam.klume.room.repository.DailyAvailableTimeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -46,24 +44,29 @@ public class MyReservationServiceImpl implements MyReservationService {
                 .orElseThrow(() -> new ReservationNotFoundException("예약이 존재하지 않습니다"));
 
         DailyReservation dailyReservation = dailyReservationRepository.findByReservation(reservation);
-        dailyReservation.cancel();
-
         int dailyAvailableTimeId = dailyReservation.getDailyAvailableTime().getId();
         DailyAvailableTime dailyAvailableTime = dailyAvailableTimeRepository.findById(dailyAvailableTimeId)
                 .orElseThrow(() -> new DailyAvailableTimeNotFoundException("해당 예약 시간 정보를 찾을 수 없습니다."));
 
-        dailyAvailableTime.reopen(); // 예약 재오픈
-
-        // 사용자가 1시간 이내 취소했을 시에 패널티 부여
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-        LocalDateTime cancelledAt = LocalDateTime.parse(dailyReservation.getCancelledAt(), formatter);
+        // 예약 시작 시간 계산
         String startStr = dailyAvailableTime.getAvailableTime().getRepeatStartDay() + " " +
                 dailyAvailableTime.getAvailableStartTime();
         LocalDateTime reservationStart = LocalDateTime.parse(startStr, formatter);
 
+        // 예약 시작 시간이 지났으면 예외 반환
+        if (LocalDateTime.now().isAfter(reservationStart)) {
+            throw new ReservationAlreadyStartedException();
+        }
+
+        // 예약 취소 가능
+        dailyReservation.cancel();
+        dailyAvailableTime.reopen();
+
+        // 사용 가능 시작 시간 한시간 이내에 취소시 패널티 부여
+        LocalDateTime cancelledAt = LocalDateTime.parse(dailyReservation.getCancelledAt(), formatter);
         if (!cancelledAt.isBefore(reservationStart.minusHours(1))) {
-            // 1시간 이내 취소면 패널티 적용
             organizationMember.applyPenalty();
         }
     }
